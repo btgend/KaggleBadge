05a4e5418980262743988bec6800ac94e2617cf4
Miguel <miguellissimo@gmail.com>
2015-08-18 13:59:35 +0200

    working signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
       # do a redirect
+      redirect_to @user
     else
       # handle unsuccessful sign up
     end
